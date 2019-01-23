af9d999f75fc85536c324ee9d9775bb04d1c24ee
gsagar <gsagar@insightworkshop.io>
2019-01-23 07:59:53 +0545

    Minor modification to this keyword lecture

--- [03] JS Classes/[00] This Keyword/script.js
+++ [03] JS Classes/[00] This Keyword/script.js
@@ -12,6 +12,10 @@ let person = {
     }
 };
 
+// can't do
+// console.log(this.person.firstName);
+// because firstName is in private scope inside an object
+
 console.log(person.fullName());
 console.log(person.nice());
 
@@ -29,7 +33,30 @@ let tesla = {
     model: "S",
     chargingTime: "1 Hour 20 Minutes",
     getDetails() {
-        return this.model + " model requires " + this.chargingTime + " to recharge."
+        return `${this.model} model requires ${this.chargingTime} to recharge.`
     }
 };
 console.log(tesla.getDetails());
+
+// console.log(this.tesla.getDetails());
+
+
+function okay(that) {
+    console.log(that);
+    that.innerHTML = "OKAY";
+}
+
+
+// this inside a function
+this.engine = "V8";
+
+const clean = function () {
+    console.log(`cleaning ${this.engine} engine...`);
+};
+
+
+// can't do this in strict mode as we've used "use strict"; at the beginning and we cannot directly use this inside a function
+// clean();
+
+// We can do this using call method
+clean.call(this);
